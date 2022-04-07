ea4391b1fa29067cdcb386a53fb29a7f4a2a8189
Bbanez <banezprod@gmail.com>
2022-04-07 13:29:15 +0200

    Use client images

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,9 +1,7 @@
 import type { BCMSMediaParsed } from '@becomes/cms-client/types';
 import type { BCMSMostImageProcessorProcessOptions } from '@becomes/cms-most/types';
 import { createBcmsImageHandler } from '@becomes/cms-most/frontend';
-import { output } from '@becomes/cms-most/frontend/_output-path';
 import * as React from 'react';
-import { FC, useEffect, useRef, useState } from 'react';
 
 interface Props {
   media: BCMSMediaParsed;
@@ -13,12 +11,18 @@ interface Props {
   options?: BCMSMostImageProcessorProcessOptions;
 }
 
-const BCMSImage: FC<Props> = ({ media, options, className, id, style }) => {
-  const handler = createBcmsImageHandler(media, options, output);
-  const [srcSet, setSrcSet] = useState(handler.getSrcSet());
-  const container = useRef<HTMLDivElement | null>(null);
+const BCMSImage: React.FC<Props> = ({
+  media,
+  options,
+  className,
+  id,
+  style,
+}) => {
+  const handler = createBcmsImageHandler(media, options);
+  const [srcSet, setSrcSet] = React.useState(handler.getSrcSet());
+  const container = React.useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
+  React.useEffect(() => {
     window.removeEventListener('resize', resizeHandler);
     const el = container.current;
     if (!el) {
@@ -42,8 +46,35 @@ const BCMSImage: FC<Props> = ({ media, options, className, id, style }) => {
   }, [media, options]);
 
   return (
-    <div id={id} className={className} style={style} ref={container}>
-      {handler.parsable ? (
+    <div
+      id={id}
+      className={className}
+      style={style}
+      ref={container}
+      data-bcms-img-w={srcSet[2]}
+      data-bcms-img-h={srcSet[3]}
+      data-bcms-img-src={media.src}
+      data-bcms-img-ops={handler.optionString}
+      data-bcms-img-idx={srcSet[4]}
+    >
+      <img
+        src={`${process.env.NEXT_PUBLIC_BCMS_ORIGIN || ''}/api/media/${
+          media._id
+        }/bin/${process.env.NEXT_PUBLIC_KEY_ID}?ops=${
+          handler.optionString
+        }&idx=${srcSet[4]}`}
+        alt={media.alt_text}
+        width={media.width}
+        height={media.height}
+      />
+      {/* <Image
+        src={'/bcms-media' + media.src}
+        alt={media.alt_text}
+        objectFit="cover"
+        width={srcSet[2].toFixed(0)}
+        height={srcSet[3].toFixed(0)}
+      /> */}
+      {/* {handler.parsable ? (
         <picture>
           <source srcSet={'/api' + srcSet[0]} />
           <source srcSet={'/api' + srcSet[1]} />
@@ -62,7 +93,7 @@ const BCMSImage: FC<Props> = ({ media, options, className, id, style }) => {
           width={media.width}
           height={media.height}
         />
-      )}
+      )} */}
     </div>
   );
 };
